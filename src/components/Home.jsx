8c249537ea29608e91317803e1e8495b51320149
Godwindaniel10 <godwinolele10@gmail.com>
2022-06-15 16:43:09 +0100

    removed console.log data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,6 @@ export default function Home() {
     if (event.key === "Enter") {
       axios.get(baseUrl).then((response) => {
         setData(response.data);
-        console.log(response.data);
       });
       setLocation("");
     }
